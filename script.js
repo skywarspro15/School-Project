b82d287b19e44544e36c883535ab2199c938214a
skwyarspro15 <potatobg648@gmail.com>
2022-10-03 07:43:10 +0800

    Countdown timer

--- script.js
+++ script.js
@@ -163,4 +163,34 @@ function getCookie(cname) {
   return "";
 }
 
+// Countdown timer
+var countDownDate = new Date("October 10, 2022 12:00:00").getTime();
+
+var x = setInterval(function () {
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("releasingIN").innerHTML =
+    "Something new is releasing in " +
+    days +
+    "d " +
+    hours +
+    "h " +
+    minutes +
+    "m " +
+    seconds +
+    "s...";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("releasingIN").innerHTML = "Released";
+  }
+}, 1000);
+
 load();

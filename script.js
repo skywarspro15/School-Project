12a8b8ba50655f0560e23e3676aa2e911b7db567
skwyarspro15 <potatobg648@gmail.com>
2022-10-03 10:59:43 +0800

    Added Discord embed and text gradient

--- script.js
+++ script.js
@@ -166,6 +166,31 @@ function getCookie(cname) {
 // Countdown timer
 var countDownDate = new Date("October 10, 2022 12:00:00").getTime();
 
+var now = new Date().getTime();
+
+var distance = countDownDate - now;
+
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+document.getElementById("releasingIN").innerHTML =
+  "Something new is releasing in " +
+  days +
+  "d " +
+  hours +
+  "h " +
+  minutes +
+  "m " +
+  seconds +
+  "s...";
+
+if (distance < 0) {
+  clearInterval(x);
+  document.getElementById("releasingIN").innerHTML = "Released";
+}
+
 var x = setInterval(function () {
   var now = new Date().getTime();
 
